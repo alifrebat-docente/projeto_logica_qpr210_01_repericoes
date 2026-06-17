ce705cd5287016458bbc2f55946fc5ee0c1f8eb4
Alisson Freire <alisson.batista@docente.senai.br>
2026-06-17 14:22:33 -0300

    Criado o algoritmo para controlar o for

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -1,7 +1,7 @@
 //ESTRUTURA DE REPETIÇÃO FOR
 const divFor = document.querySelector('#div-for')
 
-for(i = 0;i < 10; i++ ){
+for (i = 0; i < 10; i++) {
     console.log(i, 'Amor')
     divFor.innerHTML += `${i} - Amor <br> `
 }
@@ -14,9 +14,9 @@ const divContAcum = document.querySelector('#div-cont-acum')
 let cont = 0
 let acum = 0.0
 
-btnNum.addEventListener('click', (evt)=>{
+btnNum.addEventListener('click', (evt) => {
     let numDigitado = Number(inputNum.value)
-    
+
     cont++
     acum += numDigitado
 
@@ -26,3 +26,20 @@ btnNum.addEventListener('click', (evt)=>{
     inputNum.value = ''
 
 })
+
+//CONTROLANDO FOR
+const inputFrase = document.querySelector('#frase')
+const inputNumRepeticao = document.querySelector('#num-repeticao')
+const btnFrase = document.querySelector('#btn-frase')
+const divFrase = document.querySelector('#div-frase')
+
+btnFrase.addEventListener('click', (evt) => {
+    let numRepeti = Number(inputNumRepeticao.value)
+    let frase = inputFrase.value
+
+    for (i = 0; i < numRepeti; i++){
+        divFrase.innerHTML += `${i + 1} - ${frase} <br>`
+    }
+})
+
+//FOR IN
